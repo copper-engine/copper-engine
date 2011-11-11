67c397d9a33a552ddd87bb5e98b1cd64b309da64
Michael Austermann <maustermann@scoop-gmbh.de>
2011-11-11 08:09:49 +0000

    Removed obsolete logging

--- projects/copper-coreengine/src/main/java/de/scoopgmbh/copper/audit/BatchInsertIntoAutoTrail.java
+++ projects/copper-coreengine/src/main/java/de/scoopgmbh/copper/audit/BatchInsertIntoAutoTrail.java
@@ -74,7 +74,6 @@ class BatchInsertIntoAutoTrail {
 
 		@Override
 		protected void doExec(final Collection<BatchCommand<Executor, Command>> commands, final Connection con) throws Exception {
-			logger.info("database product name="+con.getMetaData().getDatabaseProductName());
 			String _stmt = "INSERT INTO COP_AUDIT_TRAIL_EVENT (SEQ_ID,OCCURRENCE,CONVERSATION_ID,LOGLEVEL,CONTEXT,WORKFLOW_INSTANCE_ID,CORRELATION_ID,MESSAGE,LONG_MESSAGE,TRANSACTION_ID) VALUES (COP_SEQ_AUDIT_TRAIL.NEXTVAL,?,?,?,?,?,?,?,?,?)";
 			if (con.getMetaData().getDatabaseProductName().equalsIgnoreCase("MYSQL")) {
 				_stmt = "INSERT INTO COP_AUDIT_TRAIL_EVENT (OCCURRENCE,CONVERSATION_ID,LOGLEVEL,CONTEXT,WORKFLOW_INSTANCE_ID,CORRELATION_ID,MESSAGE,LONG_MESSAGE,TRANSACTION_ID) VALUES (?,?,?,?,?,?,?,?,?)";
