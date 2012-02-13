0c084384737996c1f38a3cd0f1d682fb885ee295
Michael Austermann <maustermann@scoop-gmbh.de>
2012-02-13 08:19:38 +0000

    Unit test on env without oracle or mysql available

--- projects/copper-coreengine/src/test/java/de/scoopgmbh/copper/test/persistent/OraclePersistentWorkflowTest.java
+++ projects/copper-coreengine/src/test/java/de/scoopgmbh/copper/test/persistent/OraclePersistentWorkflowTest.java
@@ -15,53 +15,76 @@
  */
 package de.scoopgmbh.copper.test.persistent;
 
+import javax.sql.DataSource;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 
 public class OraclePersistentWorkflowTest extends PersistentWorkflowTest {
 	
 	private static final String DS_CONTEXT = "oracle-unittest-context.xml";
 	
+	private static boolean dbmsAvailable = false;
+	
+	static {
+		final ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(new String[] {DS_CONTEXT, "persistent-engine-unittest-context.xml", "unittest-context.xml"});
+		try {
+			DataSource ds = context.getBean(DataSource.class);
+			ds.setLoginTimeout(10);
+			ds.getConnection();
+			dbmsAvailable = true;
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			context.close();
+		}
+	}	
+	
 	public void testAsnychResponse() throws Exception {
-		super.testAsnychResponse(DS_CONTEXT);
+		if (dbmsAvailable) super.testAsnychResponse(DS_CONTEXT);
 	}
 
 	public void testAsnychResponseLargeData() throws Exception {
-		super.testAsnychResponseLargeData(DS_CONTEXT,65536);
+		if (dbmsAvailable) super.testAsnychResponseLargeData(DS_CONTEXT,65536);
 	}
 
 	public void testWithConnection() throws Exception {
-		super.testAsnychResponse(DS_CONTEXT);
+		if (dbmsAvailable) super.testAsnychResponse(DS_CONTEXT);
 	}
 	
 	public void testWithConnectionBulkInsert() throws Exception {
-		super.testWithConnectionBulkInsert(DS_CONTEXT);
+		if (dbmsAvailable) super.testWithConnectionBulkInsert(DS_CONTEXT);
 	}
 	
 	public void testTimeouts() throws Exception {
-		super.testTimeouts(DS_CONTEXT);
+		if (dbmsAvailable) super.testTimeouts(DS_CONTEXT);
 	}
 	
 	public void testMultipleEngines() throws Exception {
-		super.testMultipleEngines(DS_CONTEXT);
+		if (dbmsAvailable) super.testMultipleEngines(DS_CONTEXT);
 	}
 	
 	public void testErrorHandlingInCoreEngine() throws Exception {
-		super.testErrorHandlingInCoreEngine(DS_CONTEXT);
+		if (dbmsAvailable) super.testErrorHandlingInCoreEngine(DS_CONTEXT);
 	}
 	
 	public void testParentChildWorkflow() throws Exception {
-		super.testParentChildWorkflow(DS_CONTEXT);
+		if (dbmsAvailable) super.testParentChildWorkflow(DS_CONTEXT);
 	}	
 
 	public void testErrorKeepWorkflowInstanceInDB() throws Exception {
-		super.testErrorKeepWorkflowInstanceInDB(DS_CONTEXT);
+		if (dbmsAvailable) super.testErrorKeepWorkflowInstanceInDB(DS_CONTEXT);
 	}
 	
 	public void testErrorHandlingInCoreEngine_restartAll() throws Exception {
-		super.testErrorHandlingInCoreEngine_restartAll(DS_CONTEXT);
+		if (dbmsAvailable) super.testErrorHandlingInCoreEngine_restartAll(DS_CONTEXT);
 	}
 	
 	public void testCompressedAuditTrail() throws Exception {
-		super.testCompressedAuditTrail(DS_CONTEXT);
+		if (dbmsAvailable) super.testCompressedAuditTrail(DS_CONTEXT);
 	}
 	
 }
