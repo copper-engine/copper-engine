2f914743c1dca5f3b5b0da0c992924545a5683b0
Michael Austermann <maustermann@scoop-gmbh.de>
2012-04-05 10:26:09 +0000

    Oracle DB schema fixes

--- projects/copper-coreengine/src/main/database/oracle/create-schema.sql
+++ projects/copper-coreengine/src/main/database/oracle/create-schema.sql
@@ -1,13 +1,13 @@
 --
--- BUSINESSPROCESS
+-- COP_WORKFLOW_INSTANCE
 --
 create table COP_WORKFLOW_INSTANCE  (
-   ID           		VARCHAR2(128)                   not null,
+   ID           		VARCHAR2(128CHAR)               not null,
    STATE                NUMBER(1)                       not null,
    PRIORITY             NUMBER(2)                       not null,
    LAST_MOD_TS          TIMESTAMP                       not null,
-   PPOOL_ID      		VARCHAR2(32)					not null,
-   DATA					VARCHAR2(4000) /* CLOB */		null,
+   PPOOL_ID      		VARCHAR2(32CHAR)				not null,
+   DATA					VARCHAR2(4000CHAR)				null,
    LONG_DATA			CLOB							null,
    CS_WAITMODE			NUMBER(1),
    MIN_NUMB_OF_RESP		NUMBER(10),
@@ -19,10 +19,13 @@ create table COP_WORKFLOW_INSTANCE  (
 LOB(LONG_DATA) STORE AS SECUREFILE
 INITRANS 5;
 
+--
+-- COP_WORKFLOW_INSTANCE_ERROR
+--
 create table COP_WORKFLOW_INSTANCE_ERROR (
-   WORKFLOW_INSTANCE_ID		VARCHAR2(128)	not null,
-   EXCEPTION				CLOB			not null,
-   ERROR_TS     	   		TIMESTAMP(3)    not null
+   WORKFLOW_INSTANCE_ID		VARCHAR2(128CHAR) not null,
+   EXCEPTION				CLOB			  not null,
+   ERROR_TS     	   		TIMESTAMP(3)      not null
 );
  
 create index IDX_COP_WFID_WFID on COP_WORKFLOW_INSTANCE_ERROR (
@@ -30,33 +33,32 @@ create index IDX_COP_WFID_WFID on COP_WORKFLOW_INSTANCE_ERROR (
 );
 
 --
--- RESPONSE
+-- COP_RESPONSE
 --
 create table COP_RESPONSE  (
-   CORRELATION_ID	VARCHAR2(128) not null,
-   RESPONSE_TS		TIMESTAMP not null,
-   RESPONSE			VARCHAR2(4000),
+   CORRELATION_ID	VARCHAR2(128CHAR) 	not null,
+   RESPONSE_TS		TIMESTAMP 			not null,
+   RESPONSE			VARCHAR2(4000CHAR),
    LONG_RESPONSE	CLOB,
    constraint PK_COP_RESPONSE primary key (CORRELATION_ID) using index storage (buffer_pool keep)
 )
-LOB(LONG_RESPONSE) STORE AS SECUREFILE
-;
+LOB(LONG_RESPONSE) STORE AS SECUREFILE;
 
  
 --
--- WAIT
+-- COP_WAIT
 --
 create table COP_WAIT (
-   	CORRELATION_ID			VARCHAR2(128) not null,
-   	WORKFLOW_INSTANCE_ID  	VARCHAR2(128) not null,
-	MIN_NUMB_OF_RESP		NUMBER(10) not null,
+   	CORRELATION_ID			VARCHAR2(128CHAR) 	not null,
+   	WORKFLOW_INSTANCE_ID  	VARCHAR2(128CHAR) 	not null,
+	MIN_NUMB_OF_RESP		NUMBER(10) 			not null,
 	TIMEOUT_TS				TIMESTAMP(2),
-   	WFI_ROWID				ROWID not null,
-   	STATE					NUMBER(1) not null,
-    PRIORITY            	NUMBER(2) not null,
-    PPOOL_ID      			VARCHAR2(32) not null,
-    constraint PK_COP_WAIT primary key (CORRELATION_ID)
---   constraint FK_COP_WAIT_REFERENCE_BP foreign key (WORKFLOW_INSTANCE_ID) references COP_WORKFLOW_INSTANCE (ID)
+   	WFI_ROWID				ROWID 				not null,
+   	STATE					NUMBER(1) 			not null,
+    PRIORITY            	NUMBER(2) 			not null,
+    PPOOL_ID      			VARCHAR2(32CHAR) 	not null,
+    constraint PK_COP_WAIT primary key (CORRELATION_ID),
+    constraint FK_COP_WAIT_REFERENCE_BP foreign key (WORKFLOW_INSTANCE_ID) references COP_WORKFLOW_INSTANCE (ID)
 )
 organization index storage (buffer_pool keep);
 
@@ -67,32 +69,35 @@ create index IDX_COP_WAIT_WFI_ID on COP_WAIT (
 storage (buffer_pool keep);
 
 --
--- QUEUE
+-- COP_QUEUE
 --
 create table COP_QUEUE (
-   PPOOL_ID      		VARCHAR2(32)					not null,
+   PPOOL_ID      		VARCHAR2(32CHAR)				not null,
    PRIORITY             NUMBER(2)                       not null,
    LAST_MOD_TS          TIMESTAMP                       not null,
    WFI_ROWID			ROWID							not null,
-   ENGINE_ID			VARCHAR2(16)					null,
+   ENGINE_ID			VARCHAR2(16CHAR)				null,
    constraint PK_COP_QUEUE primary key (PPOOL_ID,PRIORITY,WFI_ROWID)
 )
 organization index 
 storage (buffer_pool keep)
 compress 2;
 
+--
+-- COP_AUDIT_TRAIL_EVENT
+--
 create table COP_AUDIT_TRAIL_EVENT (
-	SEQ_ID 					NUMBER(19) NOT NULL,
-	OCCURRENCE				TIMESTAMP NOT NULL,
-	CONVERSATION_ID 		VARCHAR2(64) NOT NULL,
-	LOGLEVEL				NUMBER(2) NOT NULL,
-	CONTEXT					VARCHAR2(128) NOT NULL,
-	INSTANCE_ID				VARCHAR2(128) NULL,
-	CORRELATION_ID 			VARCHAR2(128) NULL,
-	TRANSACTION_ID 			VARCHAR2(128) NULL,
-	MESSAGE					VARCHAR2(4000) NOT NULL,
-	LONG_MESSAGE 			CLOB NULL,
-	MESSAGE_TYPE			VARCHAR2(256) NULL
+	SEQ_ID 					NUMBER(19) 			NOT NULL,
+	OCCURRENCE				TIMESTAMP 			NOT NULL,
+	CONVERSATION_ID 		VARCHAR2(64CHAR) 	NOT NULL,
+	LOGLEVEL				NUMBER(2) 			NOT NULL,
+	CONTEXT					VARCHAR2(128CHAR) 	NOT NULL,
+	INSTANCE_ID				VARCHAR2(128CHAR) 	NULL,
+	CORRELATION_ID 			VARCHAR2(128CHAR) 	NULL,
+	TRANSACTION_ID 			VARCHAR2(128CHAR) 	NULL,
+	MESSAGE					VARCHAR2(4000CHAR) 	NOT NULL,
+	LONG_MESSAGE 			CLOB 				NULL,
+	MESSAGE_TYPE			VARCHAR2(256CHAR) 	NULL
 );
 
 CREATE SEQUENCE COP_SEQ_AUDIT_TRAIL CACHE 1000;
