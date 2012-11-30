137236cef2d11873437eca2e40ceb3830c62d3f3
Michael Austermann <maustermann@scoop-gmbh.de>
2012-11-30 10:12:14 +0000

    organized imports

--- projects/copper-coreengine/src/test/java/de/scoopgmbh/copper/audit/BatchingAuditTrailTest.java
+++ projects/copper-coreengine/src/test/java/de/scoopgmbh/copper/audit/BatchingAuditTrailTest.java
@@ -19,7 +19,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
--- projects/copper-coreengine/src/test/java/de/scoopgmbh/copper/test/persistent/BasePersistentWorkflowTest.java
+++ projects/copper-coreengine/src/test/java/de/scoopgmbh/copper/test/persistent/BasePersistentWorkflowTest.java
@@ -16,7 +16,6 @@
 package de.scoopgmbh.copper.test.persistent;
 
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
