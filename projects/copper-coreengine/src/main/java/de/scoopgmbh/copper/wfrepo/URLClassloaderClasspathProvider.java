b30f3dede42740f6a26b97ae090ea3d28013dd11
Roland Scheel <rscheel@scoop-gmbh.de>
2013-08-06 10:16:45 +0000

    add parent classloaders' classpathes

--- projects/copper-coreengine/src/main/java/de/scoopgmbh/copper/wfrepo/URLClassloaderClasspathProvider.java
+++ projects/copper-coreengine/src/main/java/de/scoopgmbh/copper/wfrepo/URLClassloaderClasspathProvider.java
@@ -28,17 +28,24 @@ public class URLClassloaderClasspathProvider implements CompilerOptionsProvider 
 	@Override
 	public Collection<String> getOptions() {
 		StringBuilder buf = new StringBuilder();
-		URLClassLoader loader = (URLClassLoader) Thread.currentThread().getContextClassLoader();
-		for (URL url : loader.getURLs() ) {
-			File f = null;
-			try {
-				//convert the URL to a URI to remove the HTML encoding, if it exists.
-				f = new File(url.toURI().getPath());
-			} 
-			catch (URISyntaxException e) {
-				throw new RuntimeException("failed to convert the classpath URL '"+url+"' to a URI",e);
+		ClassLoader loader = (ClassLoader) Thread.currentThread().getContextClassLoader();
+		while (loader != null) {
+			if (loader instanceof URLClassLoader) {
+				StringBuilder buf2 = new StringBuilder();
+				for (URL url : ((URLClassLoader)loader).getURLs() ) {
+					File f = null;
+					try {
+						//convert the URL to a URI to remove the HTML encoding, if it exists.
+						f = new File(url.toURI().getPath());
+					} 
+					catch (URISyntaxException e) {
+						throw new RuntimeException("failed to convert the classpath URL '"+url+"' to a URI",e);
+					}
+					buf2.append(f.getAbsolutePath()).append(File.pathSeparator);
+				}				
+				buf.insert(0,  buf2.toString());
 			}
-			buf.append(f.getAbsolutePath()).append(File.pathSeparator);
+			loader = loader.getParent();
 		}
 		List<String> options = new ArrayList<String>();
 		options.add("-classpath");
