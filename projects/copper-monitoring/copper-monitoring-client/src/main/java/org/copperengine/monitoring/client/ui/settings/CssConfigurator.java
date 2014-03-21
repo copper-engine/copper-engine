803f5d18a655bee487dcafe56213a7b79d59d0ac
maustermann <maustermann@scoop-software.de>
2014-03-21 11:45:07 +0100

    Fixed build

--- projects/copper-monitoring/copper-monitoring-client/src/main/java/org/copperengine/monitoring/client/ui/settings/CssConfigurator.java
+++ projects/copper-monitoring/copper-monitoring-client/src/main/java/org/copperengine/monitoring/client/ui/settings/CssConfigurator.java
@@ -7,11 +7,26 @@ import java.io.Writer;
 
 import javafx.collections.ObservableList;
 
+/*
+ * Copyright 2002-2014 SCOOP Software GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 public class CssConfigurator {
     private static final String CSS_DIR = "/org/copperengine/gui/css/";
-    
+
     private final CssConfig cssConfig;
-    
+
     public CssConfigurator(CssConfig cssConfig) {
         this.cssConfig = cssConfig;
     }
@@ -26,21 +41,21 @@ public class CssConfigurator {
         stylesheets.add(baseColorsCssUrl);
 
         // theme custom colors
-        if(cssConfig.getType() != CssConfig.Type.CSS) {
+        if (cssConfig.getType() != CssConfig.Type.CSS) {
             File themeColorsFile = File.createTempFile("copper-colors-", ".css");
             themeColorsFile.deleteOnExit();
 
             String colorsCssContent = cssConfig.getColorsCssContent();
-            try(Writer cssWriter = new FileWriter(themeColorsFile)) {
+            try (Writer cssWriter = new FileWriter(themeColorsFile)) {
                 cssWriter.write(colorsCssContent);
             }
             stylesheets.add(themeColorsFile.toURI().toURL().toExternalForm());
         }
-        
+
         String baseCssUrl = this.getClass().getResource(CSS_DIR + "base.css").toExternalForm();
         stylesheets.add(baseCssUrl);
 
-        if(cssConfig.getType() == CssConfig.Type.CSS) {
+        if (cssConfig.getType() == CssConfig.Type.CSS) {
             // custom css
             stylesheets.add(cssConfig.getCssUrl());
         } else {
