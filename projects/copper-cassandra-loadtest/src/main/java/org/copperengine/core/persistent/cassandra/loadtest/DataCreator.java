0ab00380803c22d75b59bb9b2a959730b278228a
maustermann <maustermann@scoop-software.de>
2015-11-03 09:12:37 +0100

    added missing license headers

--- projects/copper-cassandra-loadtest/src/main/java/org/copperengine/core/persistent/cassandra/loadtest/DataCreator.java
+++ projects/copper-cassandra-loadtest/src/main/java/org/copperengine/core/persistent/cassandra/loadtest/DataCreator.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2002-2015 SCOOP Software GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.copperengine.core.persistent.cassandra.loadtest;
 
 import java.util.Collections;
@@ -7,7 +22,6 @@ import org.copperengine.core.Workflow;
 import org.copperengine.core.WorkflowInstanceDescr;
 import org.copperengine.core.persistent.PersistentScottyEngine;
 import org.copperengine.core.persistent.StandardJavaSerializer;
-import org.copperengine.core.persistent.cassandra.TestData;
 import org.copperengine.core.persistent.hybrid.DefaultTimeoutManager;
 import org.copperengine.core.persistent.hybrid.HybridDBStorage;
 import org.copperengine.core.persistent.hybrid.Storage;
@@ -45,10 +59,10 @@ public class DataCreator {
 
         for (int i = 0; i < 500000; i++) {
             final String id = engine.createUUID();
-            final TestData data = new TestData();
+            final LoadTestData data = new LoadTestData();
             data.id = id;
             data.someData = payload;
-            final WorkflowInstanceDescr<TestData> wfInstanceDescr = new WorkflowInstanceDescr<TestData>("org.copperengine.core.persistent.cassandra.loadtest.workflows.LoadTestWorkflow", data, id, 1, null);
+            final WorkflowInstanceDescr<LoadTestData> wfInstanceDescr = new WorkflowInstanceDescr<LoadTestData>("org.copperengine.core.persistent.cassandra.loadtest.workflows.LoadTestWorkflow", data, id, 1, null);
             engine.run(wfInstanceDescr);
         }
     }
