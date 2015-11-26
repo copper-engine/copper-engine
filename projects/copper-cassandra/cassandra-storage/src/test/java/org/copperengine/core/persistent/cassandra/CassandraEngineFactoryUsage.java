8d23ac006dc30fffde0fa76afdc13ce54395e5cf
maustermann <maustermann@scoop-software.de>
2015-11-26 13:40:39 +0100

    fixed build

--- projects/copper-cassandra/cassandra-storage/src/test/java/org/copperengine/core/persistent/cassandra/CassandraEngineFactoryUsage.java
+++ projects/copper-cassandra/cassandra-storage/src/test/java/org/copperengine/core/persistent/cassandra/CassandraEngineFactoryUsage.java
@@ -18,11 +18,9 @@ package org.copperengine.core.persistent.cassandra;
 import java.util.Arrays;
 
 import org.copperengine.core.util.PojoDependencyInjector;
-import org.junit.Test;
 
 public class CassandraEngineFactoryUsage {
 
-    @Test
     public void test() {
         CassandraEngineFactory<PojoDependencyInjector> engineFactory = new CassandraEngineFactory<PojoDependencyInjector>(Arrays.asList("package.of.copper.workflow.classes")) {
             @Override
