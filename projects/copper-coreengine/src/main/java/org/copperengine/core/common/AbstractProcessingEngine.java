1cd91d8097b65a7f94b8e03533ca3a825b91baf2
Vladyslav Gutsul <vladyslav.gutsul@scoop-software.cz>
2018-04-10 13:19:30 +0200

    * Added appClusterId to AbstractProcessingEngine to be able to group engines to one cluster

--- projects/copper-coreengine/src/main/java/org/copperengine/core/common/AbstractProcessingEngine.java
+++ projects/copper-coreengine/src/main/java/org/copperengine/core/common/AbstractProcessingEngine.java
@@ -62,6 +62,7 @@ public abstract class AbstractProcessingEngine implements ProcessingEngine, Proc
     protected Date startupTS;
     private final AtomicLong lastActivityTS = new AtomicLong(System.currentTimeMillis());
     private final EventCounter startedWorkflowInstances = new EventCounter(24*60);
+    private String appClusterId;
 
     public void setStatisticsCollector(RuntimeStatisticsCollector statisticsCollector) {
         this.statisticsCollector = statisticsCollector;
@@ -342,5 +343,13 @@ public abstract class AbstractProcessingEngine implements ProcessingEngine, Proc
     private boolean isWithin(HalfOpenTimeInterval interval, Date ts) {
         return (interval.getFrom() == null || interval.getFrom().getTime() <= ts.getTime()) &&
                (interval.getTo() == null || ts.getTime() < interval.getTo().getTime());
-    }    
+    }
+
+    public String getAppClusterId() {
+        return appClusterId;
+    }
+
+    public void setAppClusterId(String appClusterId) {
+        this.appClusterId = appClusterId;
+    }
 }
