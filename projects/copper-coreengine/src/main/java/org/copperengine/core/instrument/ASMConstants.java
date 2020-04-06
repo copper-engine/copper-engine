093eacb2ef9c14ce10dc1a10ea5d428b537372d0
Serban Iordache <serban.iordache@scoop-software.de>
2020-04-06 11:34:12 +0200

    use ASM8 as default API version

--- projects/copper-coreengine/src/main/java/org/copperengine/core/instrument/ASMConstants.java
+++ projects/copper-coreengine/src/main/java/org/copperengine/core/instrument/ASMConstants.java
@@ -6,11 +6,12 @@ public class ASMConstants {
     public static int API_VERSION = getApiVersion();
 
     private static int getApiVersion() {
-        String version = System.getProperty("org.copperengine.asm.api.version", "7").trim();
+        String version = System.getProperty("org.copperengine.asm.api.version", "8").trim();
         switch (version) {
             case "5": return Opcodes.ASM5;
             case "6": return Opcodes.ASM6;
             case "7": return Opcodes.ASM7;
+            case "8": return Opcodes.ASM8;
             default: throw new IllegalArgumentException("Unsupported ASM API version: " + version);
         }
     }
