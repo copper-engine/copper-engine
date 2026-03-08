d5c162026a79859bb3cea22a111d172171468350
Keymaster65 <wolfsvl@web.de>
2026-03-08 13:06:52 +0100

    Fix typo

--- projects/copper-coreengine/src/test/java/org/copperengine/core/wfrepo/checkpoint/CheckpointCollectorTest.java
+++ projects/copper-coreengine/src/test/java/org/copperengine/core/wfrepo/checkpoint/CheckpointCollectorTest.java
@@ -33,7 +33,7 @@ class CheckpointCollectorTest {
     @Test
     void execute() throws Exception {
 
-        // Action with Assumption, but I use Assertions tp be sure
+        // Action with Assumption, but I use Assertions to be sure
         // that test fails if something goes wrong
         TransientEngineFactory factory = new TransientEngineFactory() {
             @Override
